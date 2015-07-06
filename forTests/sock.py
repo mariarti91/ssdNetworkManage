0e4ee60a7fc262245ae9904e21cadcd31d37c579
MJ <mariarti91@gmail.com>
2015-07-06 11:30:54 +0600

    added folder for test files

---
+++ forTests/sock.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#!-*- coding:utf-8 -*-
+
+import socket
+
+sock = socket.socket()
+
+sock.bind(("", 9090))
+sock.listen(1)
+
+while True:
+	conn, addr = sock.accept()
+	print "get connection!"
+	data = conn.recv(100)
+	print "Data: " + data.decode("utf-8")
+	conn.send(b"aga,nu\n")
+	conn.close();
