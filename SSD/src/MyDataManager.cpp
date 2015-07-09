7ac8327b8fcde639156cfaa3cbc606df43a9235a
MJ <mariarti91@gmail.com>
2015-07-09 11:50:14 +0600

    added empty slot Data Handler into MyDataManagerClass

--- SSD/src/MyDataManager.cpp
+++ SSD/src/MyDataManager.cpp
@@ -1,6 +1,14 @@
 #include "MyDataManager.h"
 
+#include <QDebug>
+
 MyDataManager::MyDataManager(QObject *parent) :
     QObject(parent)
 {
+}
+//----------------------------------------------------------------------
+
+void MyDataManager::slotDataHandler(QByteArray data)
+{
+    qDebug() << "Data manager get data: " << data;
 }
