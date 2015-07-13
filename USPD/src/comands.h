e77008281c5eb3a3c18cba7c940ed512c6f7f91e
MJ <mariarti91@gmail.com>
2015-07-13 11:34:49 +0600

    changed USPD emul. added comand handler

---
+++ USPD/src/comands.h
@@ -0,0 +1,14 @@
+#ifndef COMANDS_H
+#define COMANDS_H
+
+//get energy value
+#define GET_ENERGY 49
+
+//get power value
+#define GET_POWER 50
+
+//get info value
+#define GET_INFO 51
+
+#endif // COMANDS_H
+
